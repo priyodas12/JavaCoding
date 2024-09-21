4334db6fbf7ed7bdc9d9e3d1d7f2b9c32959f79c
Priyobrato.Das <priyodas12@gmail.com>
2024-09-21 20:35:43 +0530

    java streams - grouping by and joining

--- src/main/java/tech/java/streams/StreamGroupingBy.java
+++ src/main/java/tech/java/streams/StreamGroupingBy.java
@@ -6,16 +6,34 @@ import java.util.stream.Collectors;
 public class StreamGroupingBy {
 
   /**
-   * group by with string length
+   * group by with string length and joining
    */
 
   public static void main (String[] args) {
-    var randomStringList = List.of ("AAA", "AA", "AAA", "BBB", "AAA", "C");
+    var randomStringList = List.of ("AAA", "AA", "AAA", "BBB", "A", "CCCCC");
 
-    var groupByElements = randomStringList
+    var groupByElements1 = randomStringList
         .stream ()
         .collect (Collectors.groupingBy (String::length));
 
-    groupByElements.forEach ((k, v) -> System.out.println (k + " >> " + v));
+    var groupByElements2 = randomStringList
+        .stream ()
+        .collect (
+            Collectors.groupingBy (String::length, Collectors.joining ("*")));
+
+    groupByElements1.forEach ((k, v) -> System.out.println (k + " >> " + v));
+    /**
+     * 1 >> [A]
+     2 >> [AA]
+     3 >> [AAA, AAA, BBB]
+     5 >> [CCCCC]
+     */
+    groupByElements2.forEach ((k, v) -> System.out.println (k + " >> " + v));
+    /**
+     1 >> A
+     2 >> AA
+     3 >> AAA*AAA*BBB
+     5 >> CCCCC
+     */
   }
 }
