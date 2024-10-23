78bb0900d68cc9ee98e37c1bb59230bd25959010
Priyobrato.Das <priyodas12@gmail.com>
2024-10-23 13:03:44 +0530

    stream: hashMap different operation

--- src/main/java/tech/java/streams/ArraysToHashMap.java
+++ src/main/java/tech/java/streams/ArraysToHashMap.java
@@ -11,7 +11,9 @@ public class ArraysToHashMap {
 
     var intMap1 = new HashMap<Integer, Integer> ();
     var intMap2 = new HashMap<Integer, Integer> ();
+    var intMap3 = new HashMap<Integer, Integer> ();
 
+    System.out.println ("------------getOrDefault-----------------");
     // get or default
     Arrays.stream (arr).boxed ().forEach (
         i -> intMap1.put (i, intMap1.getOrDefault (i, 0) + 1));
@@ -19,12 +21,29 @@ public class ArraysToHashMap {
     intMap1.forEach ((k, v) -> {
       System.out.printf (" '%d' --> %d \n", k, v);
     });
-    System.out.println ("-----------------------------");
+
+    System.out.println ("------------compute-----------------");
+    // compute
     Arrays.stream (arr).boxed ().forEach (i ->
                                               intMap2.compute (i, (key, val) -> val == null ? 1
                                                   : val + 1)
                                          );
     intMap2.forEach ((k, v) -> {
+      System.out.printf (" '%d' --> %d \n", k, v);
+    });
+
+    System.out.println ("------------computeIfAbsent-----------------");
+    // compute if absent
+    Arrays.stream (arr).boxed ().forEach (
+        i -> {
+          intMap3.computeIfAbsent (i, key -> 0);
+          intMap3.compute (i, (key, val) -> {
+            System.out.println ("computing: " + key + "->" + val);
+            return (val + 1);
+          });
+        });
+
+    intMap3.forEach ((k, v) -> {
       System.out.printf (" '%d' --> %d \n", k, v);
     });
   }
